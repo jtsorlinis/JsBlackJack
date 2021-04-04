5814ecd3618879570b705886935ccb7ce35502ed
Jason Tsorlinis <jtsorlinis@gmail.com>
2021-04-04 14:16:56 +1000

    .

--- dealer.js
+++ dealer.js
@@ -1,18 +1,18 @@
-const Player = require('./player');
+const Player = require('./player')
 
 module.exports = class Dealer extends Player {
-  constructor() {
-    super();
-    this.mPlayerNum = 'D';
-    this.mValue = 0;
+  constructor () {
+    super()
+    this.mPlayerNum = 'D'
+    this.mValue = 0
   }
 
-  resetHand() {
-    this.mHand = [];
-    this.mValue = 0;
+  resetHand () {
+    this.mHand = []
+    this.mValue = 0
   }
 
-  upCard() {
-    return this.mHand[0].mValue;
+  upCard () {
+    return this.mHand[0].mValue
   }
-};
+}
